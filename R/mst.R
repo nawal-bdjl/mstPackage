61afd518b264fb1c336917f3af0d42c9c985af94
rababkhatib <103246731+rababkhatib@users.noreply.github.com>
2024-04-10 21:40:11 +0200

    kruskal - added warning for not connected graphs

--- R/mst.R
+++ R/mst.R
@@ -113,6 +113,11 @@ kruskal_mst <- function(adj_matrix) {
     root_y <- find_root(y)
     parent[root_x] <<- root_y
   }
+
+  # Check if the graph is connected
+  if (length(edges) < num_vertices - 1) {
+    warning("Warning: Not possible to output a MST, as the given adjacency matrix does not represent a connected graph.")
+  }
   
   mst_edges <- matrix(0, nrow = num_vertices, ncol = num_vertices)
   mst_weight <- 0
